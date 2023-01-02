fc3420b057e17f3e688b4503170133110015a806
bisrat <bisrat.amtataw@gmail.com>
2023-01-02 10:05:17 -0500

    auth update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import Post from './Post'
+import {db, auth} from './firebase'
+import {makeStyles} from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
+import {Button, Input} from '@material-ui/core'
+import ImageUpload from './ImageUpload'
 
 function getModalStyle (){
   const top = 50; 
@@ -78,9 +84,116 @@ function App() {
       .catch((error) => alert (error.message))
   }
 
-  return (
-    <div>
+  const signIn =(event) =>{
+    event.preventDefault()
 
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message))
+  }
+
+  return (
+    <div className='app'>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img classname="app__headerImage"
+              src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' 
+              alt=""
+              />
+            </center>
+            <Input 
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e)=> setUsername(e.target.value)}
+              />
+            <Input 
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+              />
+    
+            <Input 
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              />
+
+            <Button type="submit" onClick={signUp}>
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </Modal>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img className='app_headerImage' src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' alt=''/>
+            </center>
+
+            <Input 
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+              />
+    
+            <Input 
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              />
+
+            <Button type="submit" onClick={signIn}>
+              Log In
+            </Button>
+          </form>
+        </div>
+      </Modal>
+      <div className='app__header'>
+        <img 
+          className='app__headerImage'
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt='' 
+          />
+
+          {user ? (
+            <Button onClick ={() => auth.signOut()}>
+              Log OUt
+            </Button>
+          ): (
+            <div className='app__loginContainer'>
+              <Button onClick={() => setOpenSignIn(true)}>Log In</Button>
+              <Button onClick={() => setOpen(true)}>Sign Up</Button>
+            </div>
+          )}
+      </div>
+
+      <div className='app__posts'> 
+        {posts.map(({id, post}) => (
+          <Post 
+            key={id}
+            postId={id}
+            user={user}
+            username={post.username}
+            caption={post.caption}
+            imageUrl={post.imageUrl}/>
+        ))}
+      </div>
+
+      <div className='app__upload'>
+        {user ?.displayName ? (
+          <ImageUpload username ={user.displayName}/>
+        ) : (
+          <h2 className='app__login__upload'>LOG INTO UPLOAD</h2>
+        )}
+      </div>
     </div>
   );
 }
