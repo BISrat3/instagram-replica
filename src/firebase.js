84393a67886e76a7fa71700c81c3b0ef4ea32f7f
bisrat <bisrat.amtataw@gmail.com>
2023-01-03 22:34:56 -0500

    dbconnection

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "",
-    authDomain: "instagram-replica-466a4.firebaseapp.com",
-    databaseURL: "https://instagram-replica-466a4-default-rtdb.firebaseio.com",
-    projectId: "instagram-replica-466a4",
-    storageBucket: "instagram-replica-466a4.appspot.com",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyDhjMjgxoe_bEHnoQGKoKn4gtPNBGJz_Ys",
+    authDomain: "instagram-replica-a6cd3.firebaseapp.com",
+    databaseURL:"https://instagram-replica-a6cd3-default-rtdb.firebaseio.com",
+    projectId: "instagram-replica-a6cd3",
+    storageBucket: "instagram-replica-a6cd3.appspot.com",
+    messagingSenderId: "522477813003",
+    appId: "1:522477813003:web:a7fbbc9a7344084ebb760e",
+    measurementId: "G-HRL80BVYP5",
 })
 
 const db = firebaseApp.firestore()
